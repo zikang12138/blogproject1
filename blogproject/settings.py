61657bdc026dcd452e8acdbee8d6282c93756747
zikang12138 <215981062@qq.com>
2021-08-28 18:19:13 +0800

    d

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
+        'NAME': 'blog', # 数据库名称
+        'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
+        'PORT': 3306, # 端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': '12345678', # 数据库密码
     }
 }
 
